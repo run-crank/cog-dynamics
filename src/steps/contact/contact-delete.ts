eab2b6bd999bc2a6fa7f941c7809d0f32caba87c
amcpanaligan <amcpanaligan@gmail.com>
2020-12-10 15:18:33 +0800

    Changed delete contact and lead messages to fail

--- src/steps/contact/contact-delete.ts
+++ src/steps/contact/contact-delete.ts
@@ -35,7 +35,7 @@ export class DeleteContact extends BaseStep implements StepInterface {
         const result = await this.client.delete(deleteRequest);
         return this.pass('Successfully deleted Contact %s', [email]);
       } else {
-        return this.error('Contact %s does not exist', [email]);
+        return this.fail('Contact %s does not exist', [email]);
       }
     } catch (e) {
       return this.error('There was a problem deleting the Contact: %s', [e.toString()]);
